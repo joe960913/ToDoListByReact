470766a7f8939133dc19bf3ee0cf409c57c2b5da
joe960913 <59116882@qq.com>
2019-10-18 16:51:23 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import './App.less'
 function App() {
+  const [thing, setThing] = useState([]);//所有的待办事项
+  const [add, addThing] = useState('');//表单输入值
+  //定义了一个方法，从数组中删除指定元素后返回删除后的数组
+  let remove = (arr, val) => {
+    let res = [];
+    for (let i of arr) {
+      if (i !== val) {
+        res.push(i);
+      }
+    }
+    return res;
+  };
+  let handleDelete = e => {
+    let val = e.target.value;
+    setThing(remove(thing, val));
+  };
+  let handleAdd = e => {
+     e.preventDefault()
+    setThing(thing.concat([add]))
+    addThing('')
+  };
+  let handleChange = e => {
+    let val = e.target.value;
+    addThing(val);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='container'>
+      <h1>TODO</h1>
+      <form onSubmit={handleAdd}>
+        <input type="text" value={add} onChange={handleChange}/>
+        <button value="Submit">add</button>
+      </form>
+      <ul>
+        {thing.map((item,index) => {
+          return (
+            <div key={index}>
+              <li type='square'>{item}</li>
+              <button value={item} onClick={handleDelete}>
+                delete
+              </button>
+            </div>
+          );
+        })}
+      </ul>
     </div>
   );
 }
